749f1da9314159ad7bfa3ffb702b31caa92a073d
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-10-07 14:26:30 +0300

    Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskInput = document.querySelector("#new-task-description");
 });
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const taskDescription = taskInput.value;
+  const newTask = document.createElement("li");
+  newTask.textContent = taskDescription;
+  const taskList = document.querySelector("#tasks");
+  taskList.appendChild(newTask);
+  taskInput.value = "";
+});
+
+const deleteButton = document.createElement("button");
+deleteButton.textContent = "Delete";
+deleteButton.addEventListener("click", function () {
+  newTask.remove();
+});
+
+newTask.appendChild(deleteButton);
+
+
